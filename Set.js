4a79f0c253eaf6c845b243788b8f741ab2b4a6af
ThugsInSuits <1036023528@qq.com>
2023-12-07 10:47:25 +0800

    first commit

---
+++ Set.js
@@ -0,0 +1,17 @@
+let removeDuplicatesFromArray = function( arr ) {
+  return [...new Set(arr)]
+}
+
+let Square = ( function() {
+  let _width = new WeakMap();
+  class Square {
+    constructor( width ) {
+    _width.set( this, width );
+  }
+  get area() {
+    let width = _width.get( this );
+    return width * width;
+  }
+}
+return Square;
+} )();
