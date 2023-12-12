a3cca78557a5b111b3c02efaadd4a8cc5739fb78
ThugsInSuits <1036023528@qq.com>
2023-12-12 16:35:44 +0800

    javascript class

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,21 +1,65 @@
 import React from 'react';
 
-const welcome = {
-  greeting:"Hey",
-  title:"React"
-}
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
 
+class Developer{
+  constructor(firstName,secondName){
+    this.firstName = firstName;
+    this.secondName = secondName;
+  }
+
+  getName(){
+    return this.firstName + this.secondName;
+  }
+}
 
 function App() {
   return (
     <div>
-      <h1>
-        {welcome.greeting} {welcome.title}
-      </h1>
-      <label htmlFor="search">Search:</label>
-      <input id="search" type="text"></input>
+      <h1>My Hacker Stories</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <hr />
+      <List />
     </div>
   );
+}
+
+function List() {
+  return (
+    <div>
+      {list.map(function (item) {
+        return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
+    </div>
+  )
 }
 
 export default App;
