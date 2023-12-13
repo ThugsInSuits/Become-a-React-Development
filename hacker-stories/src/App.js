c77ce0bfa92b3e935913b77dfb4eb2b274f30b6e
ThugsInSuits <1036023528@qq.com>
2023-12-13 19:43:19 +0800

    Refact fragments

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -62,10 +62,10 @@ function App() {
 }
 
 const Search = ({search,onSearch}) => (
-  <div>
+  <>
     <label htmlFor='search'>Search:</label>
     <input id='search' type='text' value={search} onChange={onSearch} />
-  </div>
+  </>
 );
 
 
