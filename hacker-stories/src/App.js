c989b0b34c2e0deae5e06803cd6dc49aed2641d4
ThugsInSuits <1036023528@qq.com>
2023-12-14 11:18:48 +0800

    complexe javascript in jsx

--- hacker-stories/src/App.js
+++ hacker-stories/src/App.js
@@ -1,5 +1,7 @@
-import React from 'react'
-const initialStories = [
+import React,{Component} from 'react'
+require('./App.css')
+
+const list = [
   {
     title: 'React',
     url: 'https://reactjs.org/',
@@ -18,98 +20,26 @@ const initialStories = [
   },
 ];
 
-const useSemiPersistenceState = (key, initialState) => {
-  const [value, setValue] = React.useState(localStorage.getItem(key) || initialState);
 
-  React.useEffect(() => {
-    localStorage.setItem(key, value);
-  }, [value,key])
-
-  return [value, setValue];
-}
-
-const App = () => {
-  const [searchTerm, setSearchTerm] = useSemiPersistenceState('search', 'React');
-
-  const [stories, setStories] = React.useState(initialStories);
-
-  const handleSearch = (event) => {
-    setSearchTerm(event.target.value);
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        {list.map(function(item) {
+          return (
+            <div>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
-
-  const handleRemoveItem = item => {
-    console.log(item)
-    const newStories = stories.filter(
-      story => item.objectID !== story.objectID
-    )
-    setStories(newStories);
-  }
-
-  const searchedStories = stories.filter(item => item.title.toLowerCase().includes(searchTerm.toLowerCase()));
-
-  return (
-    <div>
-      <h1>My Hacker Stories</h1>
-      <InputWithLabel
-        id="search"
-        value={searchTerm}
-        isFocused
-        onInputChange={handleSearch}
-      >
-        <strong>Search: </strong>
-      </InputWithLabel>
-      <hr />
-      <List list={searchedStories} onRemoveItem={handleRemoveItem} />
-    </div>
-  )
 }
-
-const InputWithLabel = ({ id, type = 'text', onInputChange, value, children, isFocused }) => {
-  const inputRef = React.useRef();
-  React.useEffect(() => {
-    if (isFocused) {
-      inputRef.current.focus();
-    }
-  }, [isFocused]);
-
-  return (
-    <>
-      <label htmlFor={id}>{children}</label>
-      &nbsp;
-      <input
-        ref={inputRef}
-        id={id}
-        type={type}
-        value={value}
-        onChange={onInputChange}
-      ></input>
-    </>
-  )
-}
-
-const List = ({ list, onRemoveItem }) =>
-  list.map(item => (
-    <Item
-      key={item.objectID}
-      item={item}
-      onRemoveItem={onRemoveItem}
-    />
-  ));
-
-const Item = ({item,onRemoveItem}) => (
-  <div>
-    <span>
-      <a href={item.url}>{item.title}</a>
-    </span>
-    <span>{item.author}</span>
-    <span>{item.num_comments}</span>
-    <span>{item.points}</span>
-    <span>
-      <button type="button" onClick={()=>onRemoveItem(item)}>
-        Dismiss
-      </button>
-    </span>
-  </div>
-  );
 
 export default App;
